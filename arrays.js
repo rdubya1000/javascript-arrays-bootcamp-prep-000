df40866285ddc7f9dcb5760e6de12a3b40e49e26
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-10 16:33:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -51,7 +51,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0)
+  return array.slice(0, array.length - 1)
   
 }
 
